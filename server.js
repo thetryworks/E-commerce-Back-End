8d78018406c13392e81a81ee17d56e8370f9a19f
thetryworks <natesmyth1@gmail.com>
2022-05-12 20:20:27 -0500

    got database seeded

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
