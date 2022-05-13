4648b883d3cbdca9c170398f9eb6611619c62fae
thetryworks <natesmyth1@gmail.com>
2022-05-12 21:40:04 -0500

    rest of the get requests added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      },
+    ]
+  }).then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
